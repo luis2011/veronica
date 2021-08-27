83f127ef7013b7051c829981587c16ce6749f641
luis2011 <50085609+luis2011@users.noreply.github.com>
2021-08-27 08:39:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dra. Verónica Díaz</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- Third party plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="nav-link js-scroll-trigger" href="#page-top">Dra. Verónica Díaz</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Servicios</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Por que Elegirnos?</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contacto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h3 class="text-uppercase text-white font-weight-bold">Dra. Verónica Díaz & Asociados</h3>
+                        <h4 class="text-uppercase text-white font-weight-bold">Estudio Jurídico</h4>
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 font-weight-light mb-5">Brindamos un servicio integral y de excelencia, adaptado a cada una de las necesidades de nuestros clientes.</p>
+                        <a class="btn btn-secondary btn-xl js-scroll-trigger" href="#about">Ver Mas</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h4 class="text-white mt-0">Bienvenidos</h4>
+                        <!--hr class="divider light my-4" /-->
+                        <p class="text-white-50 mb-4">Nuestro compromiso es desarrollar relaciones de largo plazo, buscando brindar el asesoramiento más apropiado en sus actividades diarias, como así también en transacciones complejas, persiguiendo siempre el logro de sus objetivos.</p>
+                        <a class="btn btn-secondary btn-xl js-scroll-trigger" href="#portfolio">Ver Servicios</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- Portfolio-->
+        <div id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpeg">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpeg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <!--div class="project-category text-white-50">Category</div-->
+                                <div class="project-name">Derecho Civil</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpeg">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpeg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <!--div class="project-category text-white-50">Category</div-->
+                                <div class="project-name">Contratos</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpeg">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpeg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <!--div class="project-category text-white-50">Category</div-->
+                                <div class="project-name" >Sucesiones</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpeg">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpeg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <!--div class="project-category text-white-50">Category</div-->
+                                <div class="project-name">Jubilaciones y Pensiones</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpeg">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpeg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <!--div class="project-category text-white-50">Category</div-->
+                                <div class="project-name">Familia</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpeg">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpeg" alt="" />
+                            <div class="portfolio-box-caption p-3">
+                                <!--div class="project-category text-white-50">Category</div-->
+                                <div class="project-name">Tributario</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container">
+                <h3 class="text-center mt-0">Por que Nuestros Servicios cuentan con:</h3>
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fas far fa-edit text-secondary mb-4"></i>
+                            <!--h3 class="h4 mb-2">tema1</h3-->
+                            <p class="text-muted mb-0">ATENCIÓN PERSONALIZADA</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fas fas fa-users text-secondary mb-4"></i>
+                            <!--h3 class="h4 mb-2">tema2</h3-->
+                            <p class="text-muted mb-0">TRABAJAMOS CON EMPRESAS Y PARTICULARES</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fas fa-check-circle text-secondary mb-4"></i>
+                            <!--h3 class="h4 mb-2">tema3</h3-->
+                            <p class="text-muted mb-0">EFICIENCIA, CELERIDAD Y RESPONSABILIDAD</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fas fa-hands-helping text-secondary mb-4"></i>
+                            <!--h3 class="h4 mb-2">tema4</h3-->
+                            <p class="text-muted mb-0">Y POR SOBRE TODO NUESRTO COMPROMISO CON UD.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- fin de servicios-->
+        <br>
+        <!-- Contact-->
+       <section id="contact">
+        <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="well well-sm">
+                <form class="form-horizontal" method="post">
+                    <fieldset>
+                        <legend class="text-left header">CONTACTO</legend>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="fname" name="nombre" type="text" placeholder="Nombre" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="lname" name="apellido" type="text" placeholder="Apellido" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="Email" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="phone" name="telefono" type="text" placeholder="Telefono" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="message" name="mensaje" placeholder="Ingresa aquí un mensaje para nosotros, nos comunicaremos a la brevedad" rows="7"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 text-left">
+                                <button type="submit" class="btn btn-secondary btn-lg" >Enviar</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                    <!-- inicio del php mail -->
+                                    <?php
+
+                                    if($_SERVER["REQUEST_METHOD"] == "POST")
+                                    {
+                                        $nombre = $_POST["nombre"] ;
+                                        $apellido = $_POST["apellido"] ;
+                                        $email = $_POST["email"] ;
+                                        $telefono = $_POST["telefono"] ;
+                                        $mensaje = $_POST["mensaje"] ;
+
+                                        if(isset($nombre))
+                                        {
+                                            if(isset($email))
+                                            {
+                                                if(isset($mensaje))
+                                                {
+                                                    $para = "diazv735@gmail.com"; //colocar aqui el correo a dodne quieres que llegue
+                                                    $asunto = "consulta";
+                                                    $cuerpo = $nombre."\n".$apellido."\n".$email."\n".$telefono."\n".$mensaje;
+                                                    $adicional = "From: noreply@jacoboamaru.com";
+
+                                                    mail($para,$asunto,$cuerpo,$adicional);
+                                                ?>
+                                                    <p>Envio exitoso</p>
+                                                <?php
+                                                }
+                                            }
+                                        }
+                                    }
+                                ?>
+                                <!-- fin del php email -->
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+    </section><!--/#contact-page-->
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container" align="center">
+                         <h6>Contacto</h6>
+                          <h6>Tel:3413309777</h6>
+                         <h6>Redes Sociales</h6>
+                        
+                            <a href="https://www.facebook.com/profile.php?id=100048978019578" target="_blank"><span class="col-md-1 col-md-offset-2 text-center"><i class="fab fa-facebook-square"></i></span></a>
+                            
+
+                            <a href=https://api.whatsapp.com/send?phone=3413309777&text=Consulta">
+                            <span class="col-md-1 col-md-offset-2 text-center"> <i class="fab fa-whatsapp"></i></a> </span>
+                                            
+                    
+                <div class="small text-center text-muted">Copyright © 2020</div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
